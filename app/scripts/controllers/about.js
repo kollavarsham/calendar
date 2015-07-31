56aa75f00b1740efd8d69b02c2de297422f268ae
Floyd Pink <imbleedingme@googlemail.com>
2015-07-30 23:06:16 -0400

    initial commit - scaffolded from yo angular

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name calendarApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the calendarApp
+ */
+angular.module('calendarApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name calendarApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the calendarApp
+ */
+angular.module('calendarApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
