2e9dae16c55264272edb974050cf7c46af1f5799
Floyd Pink <imbleedingme@googlemail.com>
2015-08-01 07:10:41 -0400

    refactoring; clean up scroll to top

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,7 +8,7 @@
  * Controller of the calendarApp
  */
 angular.module('calendarApp')
-  .controller('MainCtrl', function ($scope, $location, $anchorScroll, $filter, Calendar) {
+  .controller('MainCtrl', function ($scope, $location, $anchorScroll, $filter, $window, Calendar) {
 
     $scope.$watch('year', function (newYearValue) {
       $scope.calendar = Calendar.query({
@@ -28,6 +28,11 @@ angular.module('calendarApp')
       return ($scope.year && selectedYear.length) ? selectedYear[0].text : 'Not set';
     };
 
+    $window.onscroll = function () {
+      $scope.backToTopVisibility = angular.element(document).scrollTop() > 105;
+      $scope.$apply(); //or simply $scope.$digest();
+    };
+
     $scope.previous = function () {
       console.log('previous');
       $scope.year = $scope.year - 1;
@@ -43,5 +48,9 @@ angular.module('calendarApp')
       $location.hash(loc);
       $anchorScroll();
       $location.hash(old);
+    };
+
+    $scope.scrollToTop = function(){
+      angular.element('#scrollUp').click();
     };
   });
