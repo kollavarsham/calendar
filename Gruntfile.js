1eacda8141c8e331be6a831df3414953708ce0d7
FloydPink <imbleedingme@googlemail.com>
2015-08-19 15:12:06 -0400

    upgraded dependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,6 +20,9 @@ module.exports = function (grunt) {
     buildcontrol  : 'grunt-build-control'
   });
 
+  // Use connect 3.0 static
+  var serveStatic = require('serve-static');
+
   // Configurable paths for the application
   var appConfig = {
     app  : require('./bower.json').appPath || 'app',
@@ -83,16 +86,16 @@ module.exports = function (grunt) {
           open       : true,
           middleware : function (connect) {
             return [
-              connect.static('.tmp'),
+              serveStatic('.tmp'),
               connect().use(
                 '/bower_components',
-                connect.static('./bower_components')
+                serveStatic('./bower_components')
               ),
               connect().use(
                 '/app/styles',
-                connect.static('./app/styles')
+                serveStatic('./app/styles')
               ),
-              connect.static(appConfig.app)
+              serveStatic(appConfig.app)
             ];
           }
         }
@@ -102,13 +105,13 @@ module.exports = function (grunt) {
           port       : 9001,
           middleware : function (connect) {
             return [
-              connect.static('.tmp'),
-              connect.static('test'),
+              serveStatic('.tmp'),
+              serveStatic('test'),
               connect().use(
                 '/bower_components',
-                connect.static('./bower_components')
+                serveStatic('./bower_components')
               ),
-              connect.static(appConfig.app)
+              serveStatic(appConfig.app)
             ];
           }
         }
