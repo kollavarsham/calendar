92d9d0f3316dd9a1f4ea85f77b9d98aedab2f94a
FloydPink <imbleedingme@googlemail.com>
2015-08-20 13:00:33 -0400

    added a test faking angular.element for the scroll-to-top directive

--- test/spec/directives/scroll-to-top.js
+++ test/spec/directives/scroll-to-top.js
@@ -5,10 +5,21 @@ describe('Directive: scrollToTop', function () {
   // load the directive's module
   beforeEach(module('calendarApp'));
 
-  var element, scope;
+  var element, scope, fakeElement, selectorsClicked;
 
   beforeEach(inject(function ($rootScope) {
     scope = $rootScope.$new();
+
+    selectorsClicked = [];
+
+    fakeElement = function (selector) {
+      return {
+        click : function () {
+          selectorsClicked.push(selector);
+        }
+      };
+    };
+
   }));
 
   it('should render the element properly', inject(function ($compile) {
@@ -17,5 +28,18 @@ describe('Directive: scrollToTop', function () {
     expect(element[0].tagName).toBe('A');
     expect(element.prop('title')).toBe('Back to the top');
     expect(element.hasClass('scroll-up')).toBeTruthy();
+  }));
+
+  it('should call #scrollUp click on element click', inject(function ($compile) {
+    element = angular.element('<scroll-to-top></scroll-to-top>');
+    element = $compile(element)(scope);
+
+    spyOn(angular, 'element').and.callFake(fakeElement);
+
+    element.triggerHandler('click');
+
+    expect(angular.element).toHaveBeenCalledWith('#scrollUp');
+    expect(selectorsClicked.length).toBe(1);
+    expect(selectorsClicked).toEqual(['#scrollUp']);
   }));
 });
