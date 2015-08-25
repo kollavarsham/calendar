51e4d0bd0a014d75c2b831f3df4412f090a9147d
Hari Menon <imbleedingme@googlemail.com>
2015-08-25 01:11:46 -0400

    better symbols for new moon and full moon

--- app/scripts/directives/calendar-day.js
+++ app/scripts/directives/calendar-day.js
@@ -19,12 +19,12 @@ angular.module('calendarApp')
         // add a fullDate property to show the full date
         day.fullDate = day.date + ' ' + day.month + ' ' + day.year;
 
-        // add amavasi and purnima (darkmoon and fullmoon) into the calendar
+        // add amavasi and purnima (newmoon and fullmoon) into the calendar
         // TODO: Move the below array into utils (possibly the whole model adapter should be a new service)
         var tithis = ['പ്രഥമ', 'ദ്വിതീയ', 'തൃതിയ', 'ചതുർത്ഥി', 'പഞ്ചമി', 'ഷഷ്ഠി', 'സപ്തമി', 'അഷ്ടമി', 'നവമി', 'ദശമി', 'ഏകാദശി', 'ദ്വാദശി', 'ത്രയോദശി', 'ചതുർദശി'];
-        var LARGE_CIRCLE_UNICODE = '◯';
-        var BLACK_LARGE_CIRCLE_UNICODE = '⬤';
-        var paksa = day.paksa === 'Suklapaksa' ? LARGE_CIRCLE_UNICODE : BLACK_LARGE_CIRCLE_UNICODE;
+        var NEW_MOON_UNICODE = '🌑';
+        var FULL_MOON_UNICODE = '🌕';
+        var paksa = day.paksa === 'Suklapaksa' ? FULL_MOON_UNICODE : NEW_MOON_UNICODE;
         day.moon = day.tithi === 15 ? paksa : undefined;
         day.tithiDay = day.moon || tithis[day.tithi - 1];
       }
