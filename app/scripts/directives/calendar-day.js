2b7c2a5a0a9df06061eb41b1fa67123fe07e10cb
Floyd Pink <imbleedingme@googlemail.com>
2017-01-08 00:46:18 -0500

    fixes the disappearance of full moons. again

--- app/scripts/directives/calendar-day.js
+++ app/scripts/directives/calendar-day.js
@@ -26,7 +26,7 @@ angular.module('calendarApp')
         // add amavasi and purnima (newmoon and fullmoon) into the calendar
         var NEW_MOON_UNICODE = '🌑';
         var FULL_MOON_UNICODE = '🌕';
-        var paksa = day.paksa === 'Suklapaksa' ? FULL_MOON_UNICODE : NEW_MOON_UNICODE;
+        var paksa = day.tithi === 0 ? FULL_MOON_UNICODE : NEW_MOON_UNICODE;
         day.moon = day.tithi % 15 === 0 ? paksa : undefined;
         day.tithiDay = day.moon || utils.tithisLookup[day.tithi][scope.lang || 'ml'];
       }
